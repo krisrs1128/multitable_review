6441607097d30b7d479a72c364a01a8ad78142be
Kris Sankaran <sankaran.kris@gmail.com>
2018-10-13 20:21:08 -0400

    script to run everything

---
+++ run_all.sh
@@ -0,0 +1,22 @@
+mkdir -p chapter/figure/CCA
+mkdir -p chapter/figure/ccpna
+mkdir -p chapter/figure/coia
+mkdir -p chapter/figure/graph_lasso
+mkdir -p chapter/figure/lda_cca
+mkdir -p chapter/figure/pca
+mkdir -p chapter/figure/pca_iv
+mkdir -p chapter/figure/pmd
+mkdir -p chapter/figure/spls
+
+cd dimension_red
+Rscript pca.R
+Rscript pmd.R
+Rscript cca.R
+Rscript ccpna.R
+Rscript coia.R
+Rscript lda_cca.R
+Rscript pca_iv.R
+Rscript illustration.R
+Rscript illustration_pmd.R
+
+cd ../supervised/
